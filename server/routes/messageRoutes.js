20fa37d085e408ad2a603ef47121ed0e6e3ef5f5
Pranav Koirala <pranavkoirala7@gmail.com>
2024-11-08 12:11:49 -0800

    Add crypto dependency and implement message ID generation

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,15 +1,24 @@
 import express from "express";
 import Message from "../models/Message.js";
+import crypto from "crypto";
 
 const router = express.Router();
+
+const generateRandomId = () => {
+  return crypto.randomBytes(16).toString("hex");
+};
 
 router.post("/create", async (req, res) => {
   const { message, unlockTime } = req.body;
   try {
     const unixUnlockTime = Date.parse(unlockTime) / 1000;
-    const newMessage = new Message({ message, unlockDate: unixUnlockTime });
+    const newMessage = new Message({
+      message,
+      unlockDate: unixUnlockTime,
+      messageId: generateRandomId(),
+    });
     await newMessage.save();
-    res.status(201).json({ id: newMessage._id });
+    res.status(201).json({ id: newMessage.messageId });
   } catch (error) {
     res.status(400).json({ error: "Error creating message" });
   }
@@ -17,7 +26,7 @@ router.post("/create", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-    const messageDoc = await Message.findById(req.params.id);
+    const messageDoc = await Message.findOne({ messageId: req.params.id });
     if (!messageDoc)
       return res.status(404).json({ error: "Message not found" });
 
