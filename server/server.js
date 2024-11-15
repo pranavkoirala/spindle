e523e2e486cfaaad8f665a0f5c9dc15ee1dd190b
Pranav Koirala <pranavkoirala7@gmail.com>
2024-11-15 13:38:33 -0800

    Add fallback route with links to the application and repository

--- server/server.js
+++ server/server.js
@@ -22,9 +22,13 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-// Routes
 app.use("/api/messages", messageRoutes);
 
+app.get("*", (req, res) => {
+  res.send(
+    "Did you mean <a href='https://spindleit.netlify.app'>spindleit.netlify.app</a>? <br> Check out the repo at <a href='https://github.com/pranavkoirala/spindle'>GitHub</a>."
+  );
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}...`);
